683ccbaf7ecab0f279934b87abe9a7eb517e2ec1
Sara Park <sspark106@gmail.com>
2023-07-11 17:14:32 -0600

    feat: Update version of code without using createRoot

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,12 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import libraryIndexReducer from "./libraryIndex";
 import libraryDetailsReducer, { addBook, removeBook } from "./libraryDetails";
 
-export const store = configureStore({
+const store = configureStore({
     reducer: {
         libraryIndex: libraryIndexReducer,
         libraryDetails: libraryDetailsReducer
     }
 })
 
+export default store;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
