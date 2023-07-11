8950ddf251c09eb0c213ee1655e07d19cb2b621b
Sara Park <sspark106@gmail.com>
2023-07-11 10:13:22 -0600

    feat: Create libraryIndex slice

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { StrictMode } from 'react';
 import store from "./redux/store";
 import { Provider } from 'react-redux';
 
---
+++ src/redux/libraryIndex.ts
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const libraryIndexSlice = createSlice({
+    name: "libraryIndex",
+    initialState: [],
+    reducers: {
+        
+    }
+})
