8c4cdd0cb8c0cf66c6cfc604f65830cd1a7c4cab
Sara Park <sspark106@gmail.com>
2023-07-11 10:31:40 -0600

    feat: Add extraReducers containing cases for fetching data

--- src/redux/libraryIndex.ts
+++ src/redux/libraryIndex.ts
@@ -1,9 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchLibraries = createAsyncThunk("libraryIndex/fetchLibraries", () => {
+    return axios
+        .get("")
+        .then(response => response.data)
+})
+
+const initialState = {
+    loading: false, 
+    libraries: [], 
+    error: ""
+}
 
 export const libraryIndexSlice = createSlice({
     name: "libraryIndex",
-    initialState: [],
-    reducers: {
-        
+    initialState,
+    reducers: {},
+    extraReducers: builder => {
+        builder.addCase(fetchLibraries.pending, (state) => {
+            state.loading = true
+        })
+        builder.addCase(fetchLibraries.fulfilled, (state, action) => {
+            state.loading = false, 
+            state.libraries = action.payload, 
+            state.error= ""
+        })
+        builder.addCase(fetchLibraries.rejected, (state, action) => {
+            state.loading = false, 
+            state.libraries = [], 
+            state.error = action.error.message || "Error: Unable to fetch data"
+        })
     }
 })
