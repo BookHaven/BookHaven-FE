3cfca98584dd0e451f2c1f10232a4b03b4330791
Tiala Young <tialayoung@gmail.com>
2023-07-13 17:15:53 -0600

    feat: Write fetchBooks function

--- src/redux/books.ts
+++ src/redux/books.ts
@@ -1,7 +1,14 @@
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { useSelector } from "react-redux";
 import { RootState } from "./store";
+import axios from "axios";
 
-export const useBooks = () => {
-    return useSelector((state: RootState) => state.libraryDetails.books);
-  };
+export const fetchBooks = createAsyncThunk('bookDetails/fetchBooks', ({libraryId, bookId}: { libraryId: number, bookId: number}) => {
+  return axios 
+    .get(`https://1a07a8ed-6e06-4bd9-9cba-6790e4268ca8.mock.pstmn.io/api/v0/libraries/${libraryId}/books/${bookId}`)
+    .then(response => response.data)
+})
 
+// Create bookDetailsSlice
+// extra reducers = 3 cases for request outcomes
+// reducers = do addBook and removeBook need to live here?
