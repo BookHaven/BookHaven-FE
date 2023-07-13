93037bbb30ba9ba800af07562bc44fb0d32e14e7
Sara Park <sspark106@gmail.com>
2023-07-12 18:38:02 -0600

    feat: Add BrowserRouter in App

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import { LibraryIndexView } from './react-components/libraryIndexView';
 import { LibraryDetailsView } from './react-components/libraryDetailsView';
 import { Route, NavLink } from "react-router-dom";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <h1 className="header">Book Haven</h1>
       <NavLink to="/libraries" className="nav">View Libraries</NavLink>
-      <LibraryIndexView />
+      <Route path='/libraries' render={() => <LibraryIndexView name="libraries" />} />
       <LibraryDetailsView />
     </div>
   );
