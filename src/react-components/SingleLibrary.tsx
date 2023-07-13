808d43afed1a710f7ba06a3b083f8bfab7465925
Tiala Young <tialayoung@gmail.com>
2023-07-12 18:20:28 -0600

    feat: Create SingleLibrary file and begin component

---
+++ src/react-components/SingleLibrary.tsx
@@ -0,0 +1,8 @@
+export const SingleLibrary = ({ name, address, book_count }) => {
+  return (
+    <div className="single-library-card">
+      {/* TO DO: add NavLink */}
+      <h3 className="">{name}</h3>
+    </div>
+  )
+}
