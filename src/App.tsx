593c692f40b8f5e9f85c8e31025090589caa31fa
Sara Park <sspark106@gmail.com>
2023-07-12 19:58:10 -0600

    feat: Add link to view all libraries, and routes for LibraryIndexView and LibraryDetailsView

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import { Route, Switch, NavLink } from 'react-router-dom';
 import './App.css';
 import { LibraryIndexView } from './react-components/libraryIndexView';
 import { LibraryDetailsView } from './react-components/libraryDetailsView';
-import { Route, NavLink } from "react-router-dom";
+
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1 className="header">Book Haven</h1>
       <NavLink to="/libraries" className="nav">View Libraries</NavLink>
-      <Route path='/libraries' render={() => <LibraryIndexView name="libraries" />} />
-      <LibraryDetailsView />
+      <Switch>
+        <Route exact path='/' component={LandingPage} />
+        <Route exact path='/libraries' component={LibraryIndexView} />
+        <Route path='/libraries/:id' component={LibraryDetailsView} />
+      </Switch>
     </div>
-  );
+  )
 }
 
 export default App;
