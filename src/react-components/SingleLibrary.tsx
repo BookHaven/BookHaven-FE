94cea5945386540f31e58ae3c825f064fee585e9
Tiala Young <tialayoung@gmail.com>
2023-07-12 18:41:21 -0600

    feat: Revise JSX for how address is displayed

--- src/react-components/SingleLibrary.tsx
+++ src/react-components/SingleLibrary.tsx
@@ -10,12 +10,12 @@ interface SingleLibraryProps {
 
 export const SingleLibrary = (props: SingleLibraryProps) => {
   const { name, address, bookCount } = props;
-  
+
   return (
     <div className="single-library-container">
       {/* TO DO: add NavLink to Details page */}
       <h3 className="single-library-name">{name}</h3>
-      <p className="single-library-address">{address}</p>
+      <p className="single-library-address">{`${address.street}, ${address.city}, ${address.city}`}</p>
       <p className="single-library-count">{`${bookCount} books available`}</p>
     </div>
   )
