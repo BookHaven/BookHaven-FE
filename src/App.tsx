a203e21a3018b40ea6e4821fe68ced95dd8b7ad1
Tiala Young <tialayoung@gmail.com>
2023-07-13 15:20:30 -0600

    refactor: Update Route to include match params and component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import { LibraryIndexView } from './react-components/libraryIndexView';
 import { LibraryDetailsView } from './react-components/libraryDetailsView';
+import { BookDetailsView } from './react-components/BookDetailsView';
 
 const App: React.FC = () => {
   return (
@@ -12,7 +13,11 @@ const App: React.FC = () => {
         <Route exact path='/libraries' component={LibraryIndexView} />
         <Route path='/libraries/:id' component={LibraryDetailsView} />
         <Route path='/libraries/:id/form' />
-        <Route path='/libraries/:id/books/:book_id' />
+        <Route exact path='/libraries/:id/books/:book_id' render={({match}) => {
+          const currentBook = match.params.book_id;
+          const currentLibrary = match.params.id;
+          return <BookDetailsView currentBook={currentBook} currentLibrary={currentLibrary}/> }}
+        />
       </Switch>
     </div>
   )
