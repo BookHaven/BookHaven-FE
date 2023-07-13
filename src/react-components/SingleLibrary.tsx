7c46077823c0692c82355b01c7d83b6f339919a8
Tiala Young <tialayoung@gmail.com>
2023-07-12 18:34:33 -0600

    feat: Finish writing return JSX

--- src/react-components/SingleLibrary.tsx
+++ src/react-components/SingleLibrary.tsx
@@ -1,8 +1,10 @@
-export const SingleLibrary = ({ name, address, book_count }) => {
+export const SingleLibrary = ({ name, address, bookCount }) => {
   return (
-    <div className="single-library-card">
-      {/* TO DO: add NavLink */}
-      <h3 className="">{name}</h3>
+    <div className="single-library-container">
+      {/* TO DO: add NavLink to Details page */}
+      <h3 className="single-library-name">{name}</h3>
+      <p className="single-library-address">{address}</p>
+      <p className="single-library-count">{`${bookCount} books available`}</p>
     </div>
   )
 }
