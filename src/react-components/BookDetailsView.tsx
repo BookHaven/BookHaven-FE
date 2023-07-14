36d723e0398505b16819340a22680c9480d03935
Tiala Young <tialayoung@gmail.com>
2023-07-13 19:15:53 -0600

    refactor: Separate JSX for fulfilled case from return statement

--- src/react-components/BookDetailsView.tsx
+++ src/react-components/BookDetailsView.tsx
@@ -6,6 +6,7 @@ import { ErrorView } from './ErrorView';
 export const BookDetailsView = ({ currentBookId, currentLibraryId }: {currentBookId: number, currentLibraryId: number}) => {
   const booksDetails = useAppSelector(state => state.books);
   const dispatch = useAppDispatch();
+  let renderWhenFulfilled;
 
   const parameterObject = {
     libraryId: currentLibraryId,
@@ -18,33 +19,35 @@ export const BookDetailsView = ({ currentBookId, currentLibraryId }: {currentBoo
 
   const bookToDisplay = booksDetails.books.find(book => book.id === currentBookId);
 
+  if (bookToDisplay) {
+    renderWhenFulfilled =
+      <>
+        <img src={`${bookToDisplay.attributes.book_image}`} alt={`${bookToDisplay.attributes.title}`}/>
+        {/* TO DO: add library name - need to access Library data from store */}
+        <div>
+          <p>{bookToDisplay.attributes.title}</p>
+          <p>{bookToDisplay.attributes.author}</p>
+          <span>
+            <p>{bookToDisplay.attributes.genre}</p>
+            <p>{bookToDisplay.attributes.isbn}</p>
+          </span>
+          <p>About</p>
+          <p>{bookToDisplay.attributes.description}</p>
+        </div>
+        <div>
+          <button>Checkout Book</button>
+          <button>Book Not Here</button>
+          <button>Return to Libraries</button>
+        </div>
+      </>
+  };
+
   return (
     <div>
       <h2>Book Details</h2>
       {booksDetails.loading && <div>Loading...</div>}
       {!booksDetails.loading && booksDetails.error && <div><ErrorView error={booksDetails.error} /></div>}
-
-      {!booksDetails.loading && booksDetails.books.length && bookToDisplay && 
-        <>
-          <img src={`${bookToDisplay.attributes.book_image}`} alt={`${bookToDisplay.attributes.title}`}/>
-          {/* TO DO: add library name - need to access Library data from store */}
-          <div>
-            <p>{bookToDisplay.attributes.title}</p>
-            <p>{bookToDisplay.attributes.author}</p>
-            <span>
-              <p>{bookToDisplay.attributes.genre}</p>
-              <p>{bookToDisplay.attributes.isbn}</p>
-            </span>
-            <p>About</p>
-            <p>{bookToDisplay.attributes.description}</p>
-          </div>
-          <div>
-            <button>Checkout Book</button>
-            <button>Book Not Here</button>
-            <button>Return to Libraries</button>
-          </div>
-        </>
-      }
+      {!booksDetails.loading && booksDetails.books.length && bookToDisplay && <>{renderWhenFulfilled}</>}
     </div>
   );
 };
