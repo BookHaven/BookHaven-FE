c98b37a071eda00c1868c37970a89251fb60db3b
Tiala Young <tialayoung@gmail.com>
2023-07-14 08:18:07 -0600

    feat: Add Route for landing page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import { LibraryIndexView } from './react-components/libraryIndexView';
 import { LibraryDetailsView } from './react-components/libraryDetailsView';
+import { LandingView } from './react-components/LandingView';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1 className="header">Book Haven</h1>
       <Switch>
-        <Route exact path='/' />
+        <Route exact path='/' component={LandingView}/>
         <Route exact path='/libraries' component={LibraryIndexView} />
         <Route path='/libraries/:id' component={LibraryDetailsView} />
         <Route path='/libraries/:id/form' />
