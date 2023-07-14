3b11a059132bf9dcce248ea63697e14b34c711ee
Sara Park <sspark106@gmail.com>
2023-07-14 14:35:10 -0600

    feat: Add handleIsbnChange function to setIsbn with input

--- src/react-components/Form.tsx
+++ src/react-components/Form.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-// import { useAppSelector, useAppDispatch } from '../redux/store';
-// import { addBook } from "../redux/form";
-// import form from '../redux/form';
-// import { LibraryInfo } from './LibraryInfo';
+import React, { ChangeEvent } from 'react';
+import { useAppSelector, useAppDispatch } from '../redux/store';
+import { addBook } from "../redux/form";
+import form from '../redux/form';
+import { LibraryInfo } from './LibraryInfo';
+import books from '../redux/books';
 
-// export const FormView = (id: number) => {
-//     const libraryDetails = useAppSelector(state => state.libraryDetails);
-//     const dispatch = useAppDispatch()
-//     const newBook = 
+export const FormView = (id: number) => {
 
-//     const handleClick = () => {
-//         dispatch(addBook(newBook))
-//     }
+    interface Isbn {
+        isbn: string
+    }
 
-//     return (
-//         <div>
-//             <LibraryInfo id={id}/>
-//             <form>  
-//                 <h2>Add a book to this library</h2>  
-//                 <input type="number" placeholder='ISBN'/>
-//                 <button onClick={handleClick}>Add Book</button>
-//             </form>
-//         </div>
-//     )
-// };
+    const [isbn, setIsbn] = React.useState<Isbn>({ isbn: '' });
+
+    const libraryDetails = useAppSelector(state => state.libraryDetails);
+    const dispatch = useAppDispatch()
+
+    const handleIsbnChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setIsbn({ isbn: event.target.value });
+    };
+
+    const handleClick = () => {
+        dispatch(addBook())
+    }
+
+    return (
+        <div>
+            <LibraryInfo id={id}/>
+            <form>  
+                <h2>Add a book to this library</h2>  
+                <input type="text" placeholder='ISBN' value={isbn.isbn} onChange={handleIsbnChange}/>
+                <button onClick={handleClick}>Add Book</button>
+            </form>
+        </div>
+    )
+};
+
+// pass isbn value to addBook()
+    //trigger POST request 
+    //wait for response 
+    //add response to books array 
--- src/redux/form.ts
+++ src/redux/form.ts
@@ -1,44 +1,44 @@
 import React, { Component } from "react";
-// import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-// import { useAppSelector } from '../redux/store';
-// import { RootState } from './store';
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { useAppSelector } from '../redux/store';
+import { RootState } from './store';
 
-// interface InitialState {
-//     isbn: number, 
-//     books: Book[]
-// }
+interface InitialState {
+    isbn: number, 
+    books: Book[]
+}
 
-// interface Book {
-//     id: number, 
-//     type: string, 
-//     attributes: {
-//      isbn: number, 
-//      book_image: string, 
-//      description: string, 
-//      title: string, 
-//      author: string, 
-//      genre: string,
-//      library_id: number
-//     }
-// }
+interface Book {
+    id: number, 
+    type: string, 
+    attributes: {
+     isbn: number, 
+     book_image: string, 
+     description: string, 
+     title: string, 
+     author: string, 
+     genre: string,
+     library_id: number
+    }
+}
 
-// const libraryDetails = useAppSelector((state: RootState) => state.libraryDetails);
+const libraryDetails = useAppSelector((state: RootState) => state.libraryDetails);
 
-// const initialState: InitialState = {
-//     isbn: 0,
-//     books: libraryDetails.books
-// }
+const initialState: InitialState = {
+    isbn: 0,
+    books: libraryDetails.books
+}
 
-// export const formSlice = createSlice({
+export const formSlice = createSlice({
 
-//     name: "form",
-//     initialState,
-//     reducers: {
-//         addBook: (state, action: PayloadAction<Book>) => {
-//             state.books = [...state.books, action.payload];
-//         }
-//     }
-// })
+    name: "form",
+    initialState,
+    reducers: {
+        addBook: (state, action: PayloadAction<Book>) => {
+            state.books = [...state.books, action.payload];
+        }
+    }
+})
 
-// export default formSlice.reducer;
-// export const { addBook } = formSlice.actions;
+export default formSlice.reducer;
+export const { addBook } = formSlice.actions;
