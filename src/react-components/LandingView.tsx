12cef27306546ad9ca16ab05160a6c1e34cb5211
Tiala Young <tialayoung@gmail.com>
2023-07-14 08:17:42 -0600

    feat: Change button into a Link to libraries index page

--- src/react-components/LandingView.tsx
+++ src/react-components/LandingView.tsx
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom";
+
 export const LandingView = () => {
   return (
     <>
       {/* Add background image */}
-      <p></p>
-      <button>View Libraries</button>
+      <Link to={'/libraries'} className="landing-button">View Libraries</Link>
     </>
   )
-}
+};
