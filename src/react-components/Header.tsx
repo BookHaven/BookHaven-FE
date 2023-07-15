e06f9250af2c9c41a5de9ab6299abf185bbd9d82
Sara Park <sspark106@gmail.com>
2023-07-15 16:59:59 -0600

    feat: Remove renderButton() and move conditional rendering logic inside div for header

--- src/react-components/Header.tsx
+++ src/react-components/Header.tsx
@@ -3,15 +3,13 @@ import '../styles/Header.css';
 import { NavLink } from 'react-router-dom';
 
 function Header() {
-    let renderButton;
 
     return (
         <header className="header">
             <NavLink exact to="/">
                 <img src="https://files.slack.com/files-pri/T029P2S9M-F05H7FKSV8T/bookhaven_logo.png" alt="BookHaven logo" className="bookhaven-logo"></img>
             </NavLink>
-            <div className="return-btn-container">{renderButton}</div>
-            {((window.location.pathname === '/') || (window.location.pathname === '/libraries')) ? renderButton=null : renderButton=<button className="return-to-libraries-btn">Return to Libraries</button>}
+            <div className="return-btn-container">{((window.location.pathname === '/') || (window.location.pathname === '/libraries')) ? null : <button className="return-to-libraries-btn">Return to Libraries</button>}</div>
         </header>
     );
 };
