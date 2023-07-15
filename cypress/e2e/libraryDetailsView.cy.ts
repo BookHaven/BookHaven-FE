e5629d85458ab8a06e3c6482d5aa1e546d7ae457
Sara Park <sspark106@gmail.com>
2023-07-14 22:40:43 -0600

    feat: Add tests for displaying error message and loading

--- cypress/e2e/libraryDetailsView.cy.ts
+++ cypress/e2e/libraryDetailsView.cy.ts
@@ -1,9 +1,9 @@
 describe('Library Details page', () => {
     beforeEach(() => {
-      cy.intercept('GET', 'https://book-haven-be-29aa9bd8a3c7.herokuapp.com/api/v0/libraries/1/books', {
-        statusCode: 200,
-        fixture: 'books.json'})
-        .visit('http://localhost:3000/libraries/1')
+        cy.intercept('GET', 'https://book-haven-be-29aa9bd8a3c7.herokuapp.com/api/v0/libraries/1/books', {
+            statusCode: 200,
+            fixture: 'books.json'})
+            .visit('http://localhost:3000/libraries/1')
     })
   
     it('should go to url ending with "/libraries/1"', () => {
@@ -16,12 +16,17 @@ describe('Library Details page', () => {
             fixture: "books.json"})
             .as('fetchBooks');
       
-          cy.wait('@fetchBooks')
-          cy.get(".books-error-message").should('be.visible');
+        cy.wait('@fetchBooks')
+        cy.get(".books-error-message").should('be.visible');
     })
   
     it('should display as loading when fetching books is pending', () => {
+        cy.intercept("GET", "https://book-haven-be-29aa9bd8a3c7.herokuapp.com/api/v0/libraries/1/books", {
+            statusCode: 202,
+            fixture: "books.json"})
+            .as('fetchBooks');
       
+        cy.get(".books-loading").should('be.visible');
     })
 
     it('should display the name of the library', () => {
