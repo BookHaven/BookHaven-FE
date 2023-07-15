7cec4dada28ad123c0065735e95cdb844748baca
Sara Park <sspark106@gmail.com>
2023-07-15 17:02:19 -0600

    feat: Add returntoLibraries() that changes url to '/libraries'

--- src/react-components/Header.tsx
+++ src/react-components/Header.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import '../styles/Header.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 
 function Header() {
+    const history = useHistory();
+
+    const returntoLibraries = () => {
+        history.push(`/libraries/`);
+    }
 
     return (
         <header className="header">
             <NavLink exact to="/">
                 <img src="https://files.slack.com/files-pri/T029P2S9M-F05H7FKSV8T/bookhaven_logo.png" alt="BookHaven logo" className="bookhaven-logo"></img>
             </NavLink>
-            <div className="return-btn-container">{((window.location.pathname === '/') || (window.location.pathname === '/libraries')) ? null : <button className="return-to-libraries-btn">Return to Libraries</button>}</div>
+            <div className="return-btn-container">{((window.location.pathname === '/') || (window.location.pathname === '/libraries')) ? null : <button className="return-to-libraries-btn" onClick={returntoLibraries}>Return to Libraries</button>}</div>
         </header>
     );
 };
