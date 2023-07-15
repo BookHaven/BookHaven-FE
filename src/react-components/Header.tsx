edc9f9b4a961634715f110df48ae9ff6c58c21ff
Sara Park <sspark106@gmail.com>
2023-07-15 17:21:46 -0600

    fix: Add onClick() to return to libraries button

--- src/react-components/Header.tsx
+++ src/react-components/Header.tsx
@@ -6,15 +6,15 @@ function Header() {
     const history = useHistory();
 
     const returntoLibraries = () => {
-        history.push(`/libraries/`);
+        history.push(`/libraries`);
     }
 
     return (
         <header className="header">
             <NavLink exact to="/">
-                <img src="https://files.slack.com/files-pri/T029P2S9M-F05H7FKSV8T/bookhaven_logo.png" alt="BookHaven logo" className="bookhaven-logo"></img>
+                <img src="https://files.slack.com/files-pri/T029P2S9M-F05H7FKSV8T/bookhaven_logo.png" alt="BookHaven logo" className="bookhaven-logo" />
             </NavLink>
-            <div className="return-btn-container">{((window.location.pathname === '/') || (window.location.pathname === '/libraries')) ? null : <button className="return-to-libraries-btn" onClick={returntoLibraries}>Return to Libraries</button>}</div>
+            {((window.location.pathname === '/') || (window.location.pathname === '/libraries')) ? null : <button className="return-to-libraries-btn" onClick={returntoLibraries}>Return to Libraries</button>}
         </header>
     );
 };
