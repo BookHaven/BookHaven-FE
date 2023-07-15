117d73bf585ca7e43ee0e237801de5ac0ce60253
Tiala Young <tialayoung@gmail.com>
2023-07-15 14:04:01 -0600

    refactor: Update existing tests to match changes to display

--- cypress/e2e/booksDetailsView.cy.ts
+++ cypress/e2e/booksDetailsView.cy.ts
@@ -1,10 +1,12 @@
 describe('Book Details page', () => {
   beforeEach(() => {
-    cy.intercept('GET', 'https://book-haven-be-29aa9bd8a3c7.herokuapp.com/api/v0/libraries/1/books', {
+    cy.intercept('GET', 'https://1a07a8ed-6e06-4bd9-9cba-6790e4268ca8.mock.pstmn.io/api/v0/libraries/1/books', {
+    // cy.intercept('GET', 'https://book-haven-be-29aa9bd8a3c7.herokuapp.com/api/v0/libraries/1/books', {
       statusCode: 200,
       fixture: 'books'
     }).as('getBooks')
-    cy.intercept('GET', 'https://book-haven-be-29aa9bd8a3c7.herokuapp.com/api/v0/libraries', {
+    cy.intercept('GET', 'https://1a07a8ed-6e06-4bd9-9cba-6790e4268ca8.mock.pstmn.io/api/v0/libraries', {
+    // cy.intercept('GET', 'https://book-haven-be-29aa9bd8a3c7.herokuapp.com/api/v0/libraries', {
       statusCode: 200,
       fixture: 'libraries'
     }).as('getLibraries')
@@ -17,10 +19,12 @@ describe('Book Details page', () => {
   it('Displays details about a specific book', () => {
     cy.get('.books-image').should('have.attr', 'src', 'http://books.google.com/books/content?id=IV3s-NUYnfEC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api')
       .get('.books-image').should('have.attr', 'alt', 'Book cover')
-    cy.get('.books-library-name').should('have.text', 'Library: Mary Beth Ball')
+    cy.get('.books-library-name').should('have.text', 'Mary Beth Ball')
     cy.get('.books-title').should('have.text', 'Imbibe!')
     cy.get('.books-author').should('have.text', 'David Wondrich')
-    cy.get('.books-genre-isbn').should('have.text', 'Cooking • ISBN 9780399532870')
+    cy.get('.books-isbn').should('have.text', 'ISBN: 9780399532870')
+    cy.get('.books-genre').should('have.text', 'Genre: Cooking')
+    cy.get('.books-remove-button').should('have.text', 'Remove Book')
     cy.get('.books-desc').should('have.text', 'An informative, anecdotal history of classic American cocktails pays tribute to Jerry Thomas, the father of the American bar, in a study that includes a host of mixology lore, legends, trivia, and more than one hundred recipes for punches, cocktails, sours, fizzes, toddies, slings, and other drinks.')
   })
 
@@ -31,13 +35,15 @@ describe('Book Details page', () => {
   })
 
   it('Displays an error message if the URL isn\'t found (400 level error)', () => {
+    // update error message text
     cy.visit('http://localhost:3000/libraries/1/books/2')
       .wait('@getBooks')
     cy.get('.error-message').should('have.text', '404: Page not found. Please click the logo above to return home.')
   })
 
   it('Displays a different error message if a server error occurs (500 level error)', () => {
-    cy.intercept('GET', 'https://book-haven-be-29aa9bd8a3c7.herokuapp.com/api/v0/libraries/1/books', {
+    cy.intercept('GET', 'https://1a07a8ed-6e06-4bd9-9cba-6790e4268ca8.mock.pstmn.io/api/v0/libraries/1/books', {
+    // cy.intercept('GET', 'https://book-haven-be-29aa9bd8a3c7.herokuapp.com/api/v0/libraries/1/books', {
       statusCode: 500,
       body: {
         message: 'Internal Server Error'
@@ -49,13 +55,29 @@ describe('Book Details page', () => {
     cy.get('.error-message').should('have.text', 'We seem to be having technical issues. Please try again later.')
   })
 
-  it('Includes a button to go back to the corresponding Library Details page', () => {
-    cy.get('.books-return').click()
+  it('User can click the library name to return to that Library\'s page', () => {
+    cy.get('.books-library-name').click()
     cy.url().should('eq', 'http://localhost:3000/libraries/1')
     cy.wait('@getBooks')
   })
 
+  it.skip('User can click a button to remove this book from this library', () => {
+    cy.intercept('DELETE', 'https://1a07a8ed-6e06-4bd9-9cba-6790e4268ca8.mock.pstmn.io/api/v0/libraries/1/books/1', {
+    // cy.intercept('DELETE', 'https://book-haven-be-29aa9bd8a3c7.herokuapp.com/api/v0/libraries/1/books/1', {
+      statusCode: 200,
+      fixture: 'booksAfterSuccessfulDelete'
+    }).as('booksAfterSuccessfulDelete')
+    
+    cy.get('.books-remove-button').click()
+    cy.url().should('eq', 'http://localhost:3000/libraries/1')
+      .wait('@booksAfterSuccessfulDelete')
+    cy.get('.books-section').children().should('have.length', 4)
+  })
+
   // TO DO:
-  // User can click the Header logo to return to the Landing page
-  // Add delete flow testing for Remove Book button
+  it.skip('User can click the Header logo to return to the Landing page', () => {
+    // revise using updated Header button
+    cy.get('.books-return').click()
+    cy.url().should('eq', 'http://localhost:3000')
+  })
 })
