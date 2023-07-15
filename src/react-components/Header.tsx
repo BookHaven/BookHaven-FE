34f15a0628e866ca9ec36177a4f0692e00b9ffe0
Sara Park <sspark106@gmail.com>
2023-07-15 15:26:24 -0600

    feat: Add conditional rendering for Return to Libraries button

--- src/react-components/Header.tsx
+++ src/react-components/Header.tsx
@@ -3,11 +3,16 @@ import '../react-components/Headers.css';
 import { NavLink } from 'react-router-dom';
 
 function Header() {
+    let renderButton;
 
     return (
-        <NavLink to="/">
-            <h1 className="header">Book Haven</h1>
-        </NavLink>
+        <>
+            <NavLink exact to="/">
+                <img src="https://files.slack.com/files-pri/T029P2S9M-F05H7FKSV8T/bookhaven_logo.png" alt="BookHaven logo"></img>
+            </NavLink>
+            <div className="return-to-libraries-btn">{renderButton}</div>
+            {((window.location.pathname === '/') || (window.location.pathname === '/libraries')) ? renderButton=null : renderButton=<button>Return to Libraries</button>}
+        </>
     );
 };
 
--- src/react-components/LandingView.tsx
+++ src/react-components/LandingView.tsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import Header from './Header';
 
 export const LandingView = () => {
   return (
     <>
       {/* Add background image */}
+      <Header />
       <Link to={'/libraries'} className="landing-button">See All Libraries</Link>
     </>
   )
