cf45a048204d490b637c4a9c785d8f1a0508a24b
Tiala Young <tialayoung@gmail.com>
2023-07-14 22:23:41 -0600

    feat: Assert page display for happy and sad paths

--- cypress/e2e/booksDetailsView.cy.ts
+++ cypress/e2e/booksDetailsView.cy.ts
@@ -4,25 +4,51 @@ describe('Book Details page', () => {
       statusCode: 200,
       fixture: 'books'
     }).as('getBooks')
-
-    cy.visit('http://localhost:3000/libraries/1')
-    cy.wait('@getBooks')
-  });
-
-  it('User can click on a specific book on the Library Details page to navigate to a Book Details page', () => {
-    cy.get('.book-link').first().click()
-    cy.url().should('eq', 'http://localhost:3000/libraries/1/books/1')
-
-    cy.intercept('GET', 'https://book-haven-be-29aa9bd8a3c7.herokuapp.com/api/v0/libraries/1/books', {
-      statusCode: 200,
-      fixture: 'books'
-    }).as('getBooks')
     cy.intercept('GET', 'https://book-haven-be-29aa9bd8a3c7.herokuapp.com/api/v0/libraries', {
       statusCode: 200,
       fixture: 'libraries'
     }).as('getLibraries')
+    
+    cy.visit('http://localhost:3000/libraries/1/books/1')
     cy.wait('@getBooks')
       .wait('@getLibraries')
+  });
+
+  // it('User can click on a specific book on the Library Details page to navigate to a Book Details page', () => {
+  //   cy.get('.book-link').first().click()
+  //   cy.url().should('eq', 'http://localhost:3000/libraries/1/books/1')
+
+  //   cy.intercept('GET', 'https://book-haven-be-29aa9bd8a3c7.herokuapp.com/api/v0/libraries/1/books', {
+  //     statusCode: 200,
+  //     fixture: 'books'
+  //   }).as('getBooks')
+  //   cy.intercept('GET', 'https://book-haven-be-29aa9bd8a3c7.herokuapp.com/api/v0/libraries', {
+  //     statusCode: 200,
+  //     fixture: 'libraries'
+  //   }).as('getLibraries')
+  //   cy.wait('@getBooks')
+  //     .wait('@getLibraries')
+  // })
+
+  it('Displays details about a specific book', () => {
+    cy.get('.books-image').should('have.attr', 'src', 'http://books.google.com/books/content?id=IV3s-NUYnfEC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api')
+      .get('.books-image').should('have.attr', 'alt', 'Book cover')
+    cy.get('.books-library-name').should('have.text', 'Library: Mary Beth Ball')
+    cy.get('.books-title').should('have.text', 'Imbibe!')
+    cy.get('.books-author').should('have.text', 'David Wondrich')
+    cy.get('.books-genre-isbn').should('have.text', 'Cooking • ISBN 9780399532870')
+    cy.get('.books-desc').should('have.text', 'An informative, anecdotal history of classic American cocktails pays tribute to Jerry Thomas, the father of the American bar, in a study that includes a host of mixology lore, legends, trivia, and more than one hundred recipes for punches, cocktails, sours, fizzes, toddies, slings, and other drinks.')
+  })
+
+  it('Displays details about a different book, depending on the URL', () => {
+    cy.visit('http://localhost:3000/libraries/1/books/3')
+    cy.get('.books-image').should('have.attr', 'src', 'http://books.google.com/books/content?id=Rtk8PgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api')
+    cy.get('.books-title').should('have.text', 'One Hundred Years of Solitude')
+  })
+
+  it('Displays an error message if the URL isn\'t found', () => {
+    cy.visit('http://localhost:3000/libraries/1/books/2')
+    cy.get('.error-message').should('have.text', '404: Page not found. Please click the logo above to return home.')
   })
 
   it.skip('', () => {
@@ -36,30 +62,11 @@ describe('Book Details page', () => {
   it.skip('', () => {
     
   })
-
-  it.skip('', () => {
-    
-  })
-
-  it.skip('', () => {
-    
-  })
-
-  it.skip('', () => {
-    
-  })
-
-  // Load fixture data
-
-  // User can navigate to this page to see book details by clicking on a specific book on the Library Details page
-  // -> assert page details
-  // If user clicks a different book on the Library Details page, they will see different book details
-  // -> assert subset of page details
-  // If user visits an incorrect URL, they will see an error message
 
   // TO DO:
   // Return to Libraries button
+  // User can click the Header logo to return to the Libraries Index page
   // Add delete flow testing for Check out book button
   // Add delete flow testing for Book not here button
-  // User can click the Header logo to return to the Libraries Index page
+
 })
