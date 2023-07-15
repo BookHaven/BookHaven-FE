b0b3ccdf134397b63d395e1e264375f756ad5e69
Sara Park <sspark106@gmail.com>
2023-07-15 15:29:57 -0600

    feat: Add semantic HTML element of header instead of empty <>

--- src/react-components/Header.tsx
+++ src/react-components/Header.tsx
@@ -1,18 +1,18 @@
 import React from "react";
-import '../react-components/Headers.css';
+import '../
 import { NavLink } from 'react-router-dom';
 
 function Header() {
     let renderButton;
 
     return (
-        <>
+        <header>
             <NavLink exact to="/">
-                <img src="https://files.slack.com/files-pri/T029P2S9M-F05H7FKSV8T/bookhaven_logo.png" alt="BookHaven logo"></img>
+                <img src="https://files.slack.com/files-pri/T029P2S9M-F05H7FKSV8T/bookhaven_logo.png" alt="BookHaven logo" className="bookhaven-logo"></img>
             </NavLink>
             <div className="return-to-libraries-btn">{renderButton}</div>
             {((window.location.pathname === '/') || (window.location.pathname === '/libraries')) ? renderButton=null : renderButton=<button>Return to Libraries</button>}
-        </>
+        </header>
     );
 };
 
