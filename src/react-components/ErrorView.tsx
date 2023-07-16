48ed9bb5eceed6e95015a7624b64c607380a59fa
Thomas Hawley <thawley22@turing.edu>
2023-07-15 18:22:36 -0600

    refactor: Update ErrorView to display error message.

--- src/react-components/ErrorView.tsx
+++ src/react-components/ErrorView.tsx
@@ -1,3 +1,7 @@
+import { Link } from "react-router-dom";
+import '../styles/errorView.css';
+import { NavLink, useHistory } from 'react-router-dom';
+
 export const ErrorView = ({ error }: {error: string}) => {
   let displayedError;
 
@@ -5,8 +9,32 @@ export const ErrorView = ({ error }: {error: string}) => {
     // TO DO: update error message text
     displayedError = "We seem to be having technical issues. Please try again later."
   } else {
-    displayedError = "404: Page not found. Please click the logo above to return home."
+    displayedError = "We can't seem to find the page you're looking for. You can head over to our homepage or recheck if you used the right address."
+    displayErrorImage = <img src="/404.png" alt="404"/> 
   }
 
-  return <p className="error-message">{displayedError}</p>
+  const history = useHistory();
+
+  const returnToHome = () => {
+    history.push(`/`);
+  }
+
+return (
+ <>
+    <div className="error-container">
+      <div className="error-foreground">
+        <div className="error-text">
+          <img src="/404.png" alt="404" className="error-title"/> 
+          <h1 className="error-title">Oops, sorry!</h1>
+          <p className="error-desc">{displayedError}</p>  
+          <button className="return-to-books-btn" onClick={returnToHome}>Return to Home</button>
+        </div>
+        <img src="/library.png" alt="Free Library" className="library-image"/>
+      </div>
+      <img src="/grass.png" alt="Grass" className="grass-image"/> 
+    </div>
+  </> 
+  )
+  // return <p className="error-message">{displayedError}</p>
 };
+
