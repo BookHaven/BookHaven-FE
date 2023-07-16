473a4677d2e721a4f99bbe952645e042cfb2af0e
Tiala Young <tialayoung@gmail.com>
2023-07-15 19:12:15 -0600

    fix: Refactor button conditional to fix bug of it showing on pages supposed to be excluded

--- src/react-components/Header.tsx
+++ src/react-components/Header.tsx
@@ -1,20 +1,27 @@
-import React from "react";
 import '../styles/Header.css';
-import { NavLink, useHistory } from 'react-router-dom';
+import { NavLink, useHistory, useLocation } from 'react-router-dom';
 
 function Header() {
     const history = useHistory();
+    const location = useLocation();
+    let checkCurrentPath;
 
     const returntoLibraries = () => {
         history.push(`/libraries`);
-    }
+    };
+
+    if (location.pathname === '/' || location.pathname === '/libraries') {
+        checkCurrentPath = null
+    } else {
+        checkCurrentPath = <button className="return-to-libraries-btn" onClick={returntoLibraries}>Return to Libraries</button>
+    };
 
     return (
         <header className="header">
             <NavLink exact to="/">
                 <img src="/bookhaven_logo.png" alt="BookHaven logo" className="bookhaven-logo"></img>
             </NavLink>
-            {((window.location.pathname === '/') || (window.location.pathname === '/libraries')) ? null : <button className="return-to-libraries-btn" onClick={returntoLibraries}>Return to Libraries</button>}
+            {checkCurrentPath}
         </header>
     );
 };
