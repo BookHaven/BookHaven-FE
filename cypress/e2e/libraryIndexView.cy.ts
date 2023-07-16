09214a5f86484a1e89870f92647cc2128f61655d
Tiala Young <tialayoung@gmail.com>
2023-07-15 19:46:53 -0600

    refactor: Update tests to reflect updated display

--- cypress/e2e/libraryIndexView.cy.ts
+++ cypress/e2e/libraryIndexView.cy.ts
@@ -12,15 +12,15 @@ describe('Library Index page', () => {
 
   it('Displays a list of library locations with book counts', () => {
     cy.get('.libraries-container').children().should('have.length', '4')
-    cy.get('.single-library-link').first().should('have.text', 'Mary Beth Ball')
+    cy.get('.single-library-name').first().should('have.text', 'Mary Beth Ball')
       .get('.single-library-address').first().should('have.text', '1748 S. Washington SteetDenver, CO 80210')
       .get('.single-library-count').first().should('have.text', '11')
-    cy.get('.single-library-link').last().should('have.text', 'Karrey Van Sky')
+    cy.get('.single-library-name').last().should('have.text', 'Karrey Van Sky')
       .get('.single-library-address').last().should('have.text', '1129 Lilac StreetBroomfield, CO 80020')
       .get('.single-library-count').last().should('have.text', '7')
   })
 
-  it('User can click on a Library name to be take to a Library Details page', () => {
+  it('User can click on a Library card to be taken to a Library Details page', () => {
     cy.intercept('GET', 'https://1a07a8ed-6e06-4bd9-9cba-6790e4268ca8.mock.pstmn.io/api/v0/libraries/1/books', {
     // cy.intercept('GET', 'https://book-haven-be-29aa9bd8a3c7.herokuapp.com/api/v0/libraries/1/books', {
       statusCode: 200,
@@ -32,7 +32,7 @@ describe('Library Index page', () => {
       .url().should('eq', 'http://localhost:3000/libraries/1')
   })
 
-  it('User can click on a different Library name to be take to a different Library Details page', () => {
+  it('User can click on a different Library card to be taken to a different Library Details page', () => {
     cy.intercept('GET', 'https://1a07a8ed-6e06-4bd9-9cba-6790e4268ca8.mock.pstmn.io/api/v0/libraries/4/books', {
     // cy.intercept('GET', 'https://book-haven-be-29aa9bd8a3c7.herokuapp.com/api/v0/libraries/4/books', {
       statusCode: 200,
@@ -55,7 +55,7 @@ describe('Library Index page', () => {
 
     cy.visit('http://localhost:3000/libraries')
       .wait('@getLibraries')
-    cy.get('.error-message').should('have.text', 'We seem to be having technical issues. Please try again later.')
+    cy.get('.error-desc').should('have.text', 'We seem to be having technical issues. Please try again later.')
   })
 
   it('User can click the displayed Header logo to return to the Landing page', () => {
